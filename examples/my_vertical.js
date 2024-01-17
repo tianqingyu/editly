e8e5db7edff5bfa9ae3e6f9988808061e6018662
ruohe <god.yangjun@163.com>
2024-01-17 12:46:13 +0800

    feat: demo

--- examples/my_vertical.js
+++ examples/my_vertical.js
@@ -7,37 +7,37 @@ const resolution = 'bd';
 const aspectRatio = '9:16';
 const videos = [
   {
-    wavUrl: './input/1.wav',
+    wavUrl: './examples/input/1.wav',
     srtTexts: '1\n00:00:00,000 --> 00:00:02,998\n时泽宇同样在关注着今天的比赛\n\n2\n00:00:02,998 --> 00:00:05,997\n先后输给龙空空和子桑琉荧之后\n\n3\n00:00:05,997 --> 00:00:10,120\n他整个人的气质似乎都发生了一些变化\n\n4\n00:00:10,120 --> 00:00:11,244\n相比于之前\n\n5\n00:00:11,244 --> 00:00:12,744\n显得内敛了许多\n\n',
     mediaUrls: [
-      './input/1.png',
+      './examples/input/1.png',
     ],
     effectName: 'zoom-in',
     wavDuration: 12.744,
   },
   {
-    wavUrl: './input/2.wav',
+    wavUrl: './examples/input/2.wav',
     srtTexts: '1\n00:00:00,000 --> 00:00:03,024\n屿桐柔声道，这是你的比赛\n\n',
     mediaUrls: [
-      './input/2.png',
+      './examples/input/2.png',
     ],
     effectName: 'zoom-out',
     wavDuration: 3.024,
   },
   {
-    wavUrl: './input/3.wav',
+    wavUrl: './examples/input/3.wav',
     srtTexts: '1\n00:00:00,000 --> 00:00:02,712\n我来那就不是真正的你了\n\n',
     mediaUrls: [
-      './input/3.png',
+      './examples/input/3.png',
     ],
     effectName: 'pan-left',
     wavDuration: 2.712,
   },
   {
-    wavUrl: './input/4.wav',
+    wavUrl: './examples/input/4.wav',
     srtTexts: '1\n00:00:00,000 --> 00:00:02,088\n屿桐淡淡地开口\n\n',
     mediaUrls: [
-      './input/4.png',
+      './examples/input/4.png',
     ],
     effectName: 'pan-right',
     wavDuration: 2.088,
@@ -68,7 +68,7 @@ editly({
   width: resCconfig[0],
   height: resCconfig[1],
   fps: 30,
-  outPath: './output/vertical_video.mp4',
+  outPath: './examples/output/vertical_video.mp4',
   defaults: {
     transition: {
       name: transitions[0],
